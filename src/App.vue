10d38dab8601994ca1bb9a380ceeb163bc7c7e1b
Vajith Chamuditha <vajithc@gmail.com>
2021-05-17 23:33:15 +0530

    project uploaded

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos= "todos" v-on:del-todo= "deleteTodo"/>
+  </div>
+</template>
+
+<script>
+
+import Todos from './components/Todos'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data(){
+    return{
+      todos: [
+        {
+          id: 1,
+          title: "Todo one",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo three",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      this.todos= this.todos.filter(todo=> todo.id !== id);
+      
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
---
+++ src/components/layout/header.vue
@@ -0,0 +1,5 @@
+<template>
+    <header class='header'>
+        <h1>Todo List</h1>
+    </header>
+</template>
